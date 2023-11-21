f5eddd1a4ee4ac53f26d96c793d13461557d9575
Dan Bachar <dan@bachar.co>
2023-11-21 18:23:02 +0100

    add database url

--- backend/config/ormconfig.ts
+++ backend/config/ormconfig.ts
@@ -4,16 +4,30 @@ import { Photo } from "src/entities/photo.entity";
 import { Project } from "src/entities/project.entity";
 import { DataSource } from "typeorm";
 
-let connectDB = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'knitting',
-    password: 'knitting',
-    database: 'knitting',
-    entities: [Project, Photo, Pattern],
-    logging: false,
-    synchronize: true,
-});
+let connectDB: DataSource;
+const databaseURL = process.env.DATABASE_URL;
+if (databaseURL == null) {
+    connectDB = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'knitting',
+        password: 'knitting',
+        database: 'knitting',
+        entities: [Project, Photo, Pattern],
+        logging: false,
+        synchronize: true,
+    });
+} else {
+    connectDB = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        url: databaseURL,
+        entities: [Project, Photo, Pattern],
+        logging: false,
+        synchronize: true,
+    });
+}
 
 export default connectDB;
