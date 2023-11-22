5d094202454a7fb9c9c3644ba4f31f9214f9f786
Dan Bachar <dan@bachar.co>
2023-11-22 08:15:25 +0100

    make prettier

--- frontend/src/components/ProjectOverview.tsx
+++ frontend/src/components/ProjectOverview.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Project } from "../entities/project";
 import { CreateProjectDialog } from "./CreateProjectDialog";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function ProjectOverview() {
     const [projects, setProjects] = useState<Project[]>([]);
@@ -32,10 +33,10 @@ export default function ProjectOverview() {
         setIsStartNewProjectDialogOpen(false);
     }
 
-
+    const isXS = useMediaQuery('(max-width:600px)');
 
     return <Grid container justifyContent="center" columnGap={6} rowSpacing={2} direction="row">
-        {projects.map((project, index) => { 
+        {projects.map((project, index) => {
             const mainPhoto = project.photos.find(photo => !!photo.featured);
 
             if (!mainPhoto) {
@@ -43,36 +44,41 @@ export default function ProjectOverview() {
             }
 
             return (
-            <Grid item xs={5} md={4} lg={3} key={project.name + "" + index}>
-                <Card sx={{ width: 200 }}>
-                    <CardMedia
-                        sx={{ height: 200 }}
-                        image={mainPhoto.filename}
-                        title={project.name}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {project.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {project.description}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Link to={"/project/" + project.id}>
-                            <Button size="small">More</Button>
-                        </Link>
-                    </CardActions>
-                </Card>
-            </Grid>
-        )})}
+                <Grid item xs={5} md={4} lg={3} key={project.name + "" + index}>
+                    <Card>
+                        <CardMedia
+                            sx={{ height: 200 }}
+                            image={mainPhoto.filename}
+                            title={project.name}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {project.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {project.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Link to={"/project/" + project.id}>
+                                <Button size="small">More</Button>
+                            </Link>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            )
+        })}
         {
             <Grid item xs={5} md={4} lg={3} key="createNew">
                 <Card>
                     <CardActions>
-                        <IconButton onClick={handleStartNewProjectClick}>
-                            <AddCircleIcon style={{ fontSize: 140, color: 'gray' }} />
-                        </IconButton>
+                        <Grid container>
+                            <Grid justifyContent="center" justifyItems="center" justifySelf="center">
+                                <IconButton onClick={handleStartNewProjectClick}>
+                                    <AddCircleIcon style={{ fontSize: isXS ? 140 : 307, color: 'gray' }} />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
                     </CardActions>
                 </Card>
             </Grid>
